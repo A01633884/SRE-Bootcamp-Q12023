4b6e6e319fa7d01b55d9adff553a5634a30e787f
Uriel Madrigal <A01633884@tec.mx>
2023-02-08 21:54:50 -0600

    Database Connection

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,4 +1,24 @@
-# These functions need to be implemented
+import mysql.connector
+import boto3
+import os
+import jwt
+
+ENDPOINT = "sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com"
+PORT = "3306"
+USER = "secret"
+REGION = "us-east-1"
+DBNAME = "bootcamp_tht"
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+
+
+try:
+    conn = mysql.connector.connect(
+        host=ENDPOINT, user=USER, passwd="jOdznoyH6swQB9sTGdLUeeSrtejWkcw", port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
+except Exception as e:
+    print("Database connection failed due to {}".format(e))
+    
 class Token:
 
     def generate_token(self, username, password):
